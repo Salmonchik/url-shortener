0d4d0549e00ca7bec4cfb6a06cf08f34ad0b190b
salmon <salikmahmudov54@gmail.com>
2023-01-27 13:33:16 +0500

    .env created

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,4 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DOMAIN = 'http://127.0.0.1:8000'
+
+DOMAIN = os.environ.get("DOMAIN")
